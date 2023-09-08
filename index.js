7367ba57902f94020187a874df70d1b2d5a4c885
IGOR-DESKTOP\igora <igorag314@gmail.com>
2023-09-07 22:51:03 -0300

    Create project using nest and prisma

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-
-const app = express();
-
-app.listen(3000, () => {
-    console.log('Servidor iniciado na porta 3000');
-});
