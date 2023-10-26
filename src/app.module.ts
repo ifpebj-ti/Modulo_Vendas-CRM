8b0a48d590cac3e11bb9e5ba67794da4b7653b16
Monique Neto <58047194+MoniqueNeto@users.noreply.github.com>
2023-10-26 20:16:18 -0300

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { FakeController } from './fake/fake.controller';
 import { SalesModule } from './sales/sales.module';
 import { PurchasesModule } from './purchases/purchases.module';
 import { TicketModule } from './ticket/ticket.module';
+import { TicketService } from './ticket/ticket.service';
 
 @Module({
   imports: [SalesModule, PurchasesModule, TicketModule],
---
+++ test/ticket.e2e-spec.ts
@@ -0,0 +1,26 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { TicketController } from 'src/ticket/ticket.controller'; 
+import { TicketService } from 'src/ticket/ticket.service';
+
+describe('TicketController', () => {
+  let controller: TicketController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [TicketController],
+      providers: [TicketService],
+    }).compile();
+
+    controller = module.get<TicketController>(TicketController);
+  });
+
+  it('define o ticket', () => {
+    expect(controller).toBeDefined();
+  });
+
+  it('calcular o ticket medio', async () => {
+    const result = await controller.calcularTicketMedioPorMes();
+    expect(result).toBeDefined();
+  });
+  
+});
