0a9ebd3d381196255f62cbe8ec9f3f11a6e9a7bc
IGOR-DESKTOP\igora <igorag314@gmail.com>
2023-12-03 21:28:22 -0300

    feat: calculate ticket by branch and interval

--- src/ticket/ticket.service.ts
+++ src/ticket/ticket.service.ts
@@ -5,7 +5,7 @@ import { PrismaService } from '../database/prisma.service';
 export class TicketService {
   constructor(private prisma: PrismaService) {}
 
-  async calcularTicketMedioMensalPorCliente(): Promise<Array<object>> {
+  async calculateTicketAllClients(): Promise<Array<object>> {
     const resultado: Array<object> = await this.prisma.$queryRaw`
       SELECT
         c.id_cliente,
@@ -18,16 +18,33 @@ export class TicketService {
     return resultado;
   }
 
-  async vendasPorCliente(id_cliente:number){
+  async salesByClients(clientId:number){
     const resultado:  Array<object> = await this.prisma.$queryRaw`
       SELECT
         c.id_cliente,
         v.total_venda
       FROM cliente c
       LEFT JOIN venda v ON c.id_cliente = v.id_cliente
-      WHERE c.id_cliente = ${parseInt(id_cliente.toString())}
+      WHERE c.id_cliente = ${parseInt(clientId.toString())}
     `;
     return resultado; 
   }
-} 
 
+  async calculateTicketByIntervalAndBranch(
+    startDate: Date,
+    endDate: Date,
+    branchId: number,
+  ): Promise<Array<object>> {
+    const intBranch = parseInt(branchId.toString())
+
+    const resultado: Array<object> = await this.prisma.$queryRaw`
+      SELECT
+        SUM(v.total_venda) / COUNT(v.id_venda) as ticketMedio
+      FROM venda v
+      WHERE v.data BETWEEN ${startDate} AND ${endDate}
+      AND v.id_filial = ${intBranch}
+    `;
+
+    return resultado;
+  }
+}
